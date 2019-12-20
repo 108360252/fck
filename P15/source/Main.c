cf07b3b4e3b9c69e15a9c9e61ab0a18ca82c947d
Nathaniel Hii <55587330+108360252@users.noreply.github.com>
2019-12-21 00:47:00 +0800

    Add files via upload

---
+++ P15/source/Main.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	FILE *fptr1,*fptr2;
+	char ch;
+	fptr1 = fopen("C:\welcome.txt","r");
+	fptr2 = fopen("C:\output.txt","w");
+	if ((fptr1!= NULL) && (fptr2!= NULL))
+	{
+		while ((ch = getc(fptr1)) != EOF)
+			putc(ch, fptr2);
+		fclose(fptr1);
+		fclose(fptr2);
+		printf("檔案拷貝完成!!A\n");
+	}
+	if (fptr1 != NULL)
+		printf("C\n");
+	if (fptr2 != NULL)
+		printf("D\n");
+	else
+		printf("檔案開啓失敗!!B\n");
+	system("pause");
+	return 0;
+}
